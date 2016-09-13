694287e274db1c544ebb95efd59a2cf0ef6b73d2
Bruno Luiz <contact@brunoluiz.net>
2016-09-13 18:29:06 -0300

    Added the google analytics code

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import { render } from 'react-dom'
-import { Router, Route, IndexRoute, browserHistory, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import ReactGA from 'react-ga'
 
 import App from 'containers/App';
 import Player from 'containers/Player';
 import HomePage from 'containers/HomePage';
 
+ReactGA.initialize(ENV.GANALYTICS);
+
+const logPageView = () => {
+  if(process.env.NODE_ENV !== 'production') return ;
+
+  ReactGA.set({
+    page: window.location.pathname
+  });
+  ReactGA.pageview(window.location.pathname);
+}
+
+if (process.env.NODE_ENV === 'production') {
+}
+
 export default (
-  <Router history={hashHistory}>
+  <Router
+    history={hashHistory}
+    onUpdate={logPageView}
+  >
     <Route path='/' component={HomePage} />
     <Route path='/playlist' component={App}>
       <Route path=':fbid' component={Player} />
